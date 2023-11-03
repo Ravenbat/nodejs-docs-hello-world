201819b15e64989e6a45006f286aa07902b76634
Raven Oyunjargal <71726806+Ravenbat@users.noreply.github.com>
2023-11-03 15:24:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ const router = express.Router();
 
 // Hello World for index page
 app.get('/', function (req, res) {
-    return res.send("Hello World!");
+    return res.send("Hello World! ravenbaatar");
 })
 
 app.get('/api', function (req, res) {
